b62df55e0f36c411e44a13ac7d40310297e4ff5d
urjan <urjan@seas.upenn.edu>
2018-03-25 12:44:59 -0400

    Adding the presentation slide deck

--- chart_js.js
+++ chart_js.js
@@ -3,7 +3,7 @@ window.onload = function () {
 var dps = []; // dataPoints
 var chart = new CanvasJS.Chart("chartContainer", {
 	title :{
-		text: "EKG trends"
+		text: ""
 	},
 	axisY: {
 		includeZero: false
@@ -21,27 +21,15 @@ var dataLength = 80; // number of dataPoints visible at any point
 
 var updateChart = function (count) {
 
-
-
-	var e = 2.71828;
-	var a = 0.2;
-	var d = 1.4;
-	var h = 3;
-	var s = 0.05;
-	var w = 0.02;
-
 	count = count || 1;
 
 	for (var j = 0; j < count; j++) {
-		
 
-	// yVal = a*(Math.pow(e,((-(x + d)^2) / (2*w))) + Math.pow(e,((-(x - d)^2) / (2*w)))) 
-	// 		+ ((h - Math.abs(x / s) - x) * Math.pow(e,((-(7*x)^2) / 2)));
 
 		yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
 		dps.push({
 			x: xVal,
-			y: next_val
+			y: yVal
 		});
 		xVal++;
 	}
@@ -57,3 +45,64 @@ updateChart(dataLength);
 setInterval(function(){updateChart()}, updateInterval);
 
 }
+
+
+// window.onload = function () {
+
+// var dps = []; // dataPoints
+// var chart = new CanvasJS.Chart("chartContainer", {
+// 	title :{
+// 		text: "EKG trends"
+// 	},
+// 	axisY: {
+// 		includeZero: false
+// 	},      
+// 	data: [{
+// 		type: "line",
+// 		dataPoints: dps
+// 	}]
+// });
+
+// var xVal = 0;
+// var yVal = 100; 
+// var updateInterval = 1000;
+// var dataLength = 80; // number of dataPoints visible at any point
+
+// var updateChart = function (count) {
+
+
+
+// 	// var e = 2.71828;
+// 	// var a = 0.2;
+// 	// var d = 1.4;
+// 	// var h = 3;
+// 	// var s = 0.05;
+// 	// var w = 0.02;
+
+// 	count = count || 1;
+
+// 	for (var j = 0; j < count; j++) {
+		
+
+// 	// yVal = a*(Math.pow(e,((-(x + d)^2) / (2*w))) + Math.pow(e,((-(x - d)^2) / (2*w)))) 
+// 	// 		+ ((h - Math.abs(x / s) - x) * Math.pow(e,((-(7*x)^2) / 2)));
+
+// 		yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
+// 		dps.push({
+// 			x: xVal,
+// 			y: next_val
+// 		});
+// 		xVal++;
+// 	}
+
+// 	if (dps.length > dataLength) {
+// 		dps.shift();
+// 	}
+
+// 	chart.render();
+// };
+
+// updateChart(dataLength);
+// setInterval(function(){updateChart()}, updateInterval);
+
+// }
--- send_sms.py
+++ send_sms.py
@@ -4,5 +4,5 @@ account = "AC99b4a100f1abb3d020ebd7ed37323cdf"
 token = "20ac0302aa5dd35da66c1ffe5e5c26b5"
 client = Client(account, token)
 
-message = client.messages.create(to="+12159513921", from_="+12675364672",
-                                 body="Hello there George! Twilio is working! Yay!")
+message = client.messages.create(to="+12159513923", from_="+12675364672",
+                                 body="Hey Urja! I have some heart issue... I need help ASAP!!")
