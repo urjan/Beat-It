24982730580aa1c878043d53a2f223fc934a7dbf
Urja Nadibail <urja.uday@gmail.com>
2018-08-14 10:01:36 -0700

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
-account = "AC99b4a100f1abb3d020ebd7ed37323cdf"
-token = "20ac0302aa5dd35da66c1ffe5e5c26b5"
+account = "XXX"
+token = "XXX"
 client = Client(account, token)
 
-message = client.messages.create(to="+12159513923", from_="+12675364672",
+message = client.messages.create(to="+XXX", from_="+XXX",
                                  body="Hey Urja! I have some heart issue... I need help ASAP!!")
